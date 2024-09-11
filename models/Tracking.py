deedb5f65bccee583ff8c99197af38ac1e99cd3a
Oxshady <shadymahmod55@gmail.com>
2024-09-11 22:39:33 +0300

    users and Tracking

--- models/Tracking.py
+++ models/Tracking.py
@@ -1,13 +1,14 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
-from models.Base import BaseModel
+from sqlalchemy.orm import relationship,Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from models.base import BaseModel, base
 
-class Traking(BaseModel):
+class Tracking(BaseModel, base):
     """Tracking class"""
+    __tablename__ = 'tracking'
     delivery_address: Mapped['str'] = mapped_column(String(100), nullable=False)
     status: Mapped['str'] = mapped_column(String(60), nullable=False)
     order_id: Mapped['str'] = mapped_column(String(60), nullable=False)
-    
+    user_id: Mapped['str'] = mapped_column(String(60), ForeignKey('users.id'),nullable=False)
     def __init__(self, *args, **kwargs) -> None:
         """ Initialize a new Tracking object """
         super().__init__(*args, **kwargs)
--- models/storageEngine/dbEngine.py
+++ models/storageEngine/dbEngine.py
@@ -1,24 +1,33 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base import base
+
 
 class DBstorage:
     __engine = None
     __session = None
 
     def __init__(self):
-        db = 'hbnb_dev'
-        user = 'hbnb_dev'
-        passwd = 'hbnb_dev_pwd'
-        host = 'localhost'
+        db = "Egy"
+        user = "shadi"
+        passwd = "1"
+        host = "localhost"
         self.__engine = create_engine(
-            'mysql+mysqldb://{}:{}@{}/{}'.format(
-                user,
-                passwd,
-                host,
-                db),
-            pool_pre_ping=True
-            )
+            "mysql+mysqldb://{}:{}@{}/{}".format(user, passwd, host, db),
+            pool_pre_ping=True,
+        )
+
     def setup(self):
-        sfactory = sessionmaker(bind=self.__engine, expire_on_commit = False)
+        from models.tracking import Tracking
+        from models.users import User
+
+        base.metadata.create_all(self.__engine)
+        sfactory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         session = scoped_session(sfactory)
         self.__session = session()
+
+    def close(self):
+        self.__session.remove()
+
+    def get_session(self):
+        return self.__session
