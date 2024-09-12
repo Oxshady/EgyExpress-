a3114d536d2c5695a451542b65c4d575a99f8a95
Oxshady <shadymahmod55@gmail.com>
2024-09-12 18:33:16 +0300

    ^_^

--- models/Order.py
+++ models/Order.py
@@ -5,7 +5,7 @@ from typing import List
 class Order(BaseModel, base):
 	"""Order class"""
 	__tablename__ = 'orders'
-	total_price: Mapped['str'] = mapped_column(String(60), nullable=False)
+	total_price: Mapped['float'] = mapped_column(Float, nullable=False)
 	user_id: Mapped['str'] = mapped_column(String(60), ForeignKey('users.id') , nullable=False)
 	payment_id: Mapped['str'] = mapped_column(String(60), ForeignKey('payment.id') , nullable=False)
 	from models.tracking import Tracking
