49fd39055736fc8481b6c2c319e30dc6d2db99e8
Oxshady <shadymahmod55@gmail.com>
2024-09-13 18:06:03 +0300

    update db

--- models/storage/db.py
+++ models/storage/db.py
@@ -1,14 +1,45 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base import base
+from models.tracking import Tracking
+from models.Category import Category
+from models.Discount import Discount
+from models.Comb import VaiationCombination
+from models.variationsKey import VaiationKey
+from models.variationsValue import VaiationValue
+from models.Product import Product
+from models.subCategory import Subcategory
+from models.order_item import OrderItem
+from models.Order import Order
+from models.Payment import Payment
+from models.CartItem import CartItem
+from models.Cart import Cart
+from models.Review import Review
+from models.users import User
 
+mapp = {
+    "Category": Category,
+    "Subcategory": Subcategory,
+    "Discount": Discount,
+    "VaiationCombination": VaiationCombination,
+    "VaiationKey": VaiationKey,
+    "VaiationValue": VaiationValue,
+    "Product": Product,
+    "OrderItem": OrderItem,
+    "Order": Order,
+    "Payment": Payment,
+    "CartItem": CartItem,
+    "Cart": Cart,
+    "Review": Review,
+    "User": User,
+}
 
 class DBstorage:
     __engine = None
     __session = None
 
     def __init__(self):
-        db = "egye"
+        db = "SHADI"
         user = "shadi"
         passwd = "1"
         host = "localhost"
@@ -18,62 +49,24 @@ class DBstorage:
         )
 
     def setup(self):
-        from models.tracking import Tracking
-        from models.Category import Category
-        from models.Discount import Discount
-        from models.Comb import VaiationCombination
-        from models.variationsKey import VaiationKey
-        from models.variationsValue import VaiationValue
-        from models.Product import Product
-        from models.subCategory import Subcategory
-        from models.order_item import OrderItem
-        from models.Order import Order
-        from models.Payment import Payment
-        from models.CartItem import CartItem
-        from models.Cart import Cart
-        from models.Review import Review
-        from models.users import User
         base.metadata.create_all(self.__engine)
         sfactory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         session = scoped_session(sfactory)
         self.__session = session()
 
+    def filter_group(self, cls, **kwargs):
+        if str(cls) in mapp:
+            self.__session.query(mapp[str(cls)]).filter_by(**kwargs).all()
+    
+    def filter_one(self, cls, **kwargs):
+        if str(cls) in mapp:
+            self.__session.query(mapp[str(cls)]).filter_by(**kwargs).first()
+    
     def post(self, obj):
         self.__session.add(obj)
         self.__session.commit()
     
     def get_all(self, cls):
-        from models.tracking import Tracking
-        from models.Category import Category
-        from models.Discount import Discount
-        from models.Comb import VaiationCombination
-        from models.variationsKey import VaiationKey
-        from models.variationsValue import VaiationValue
-        from models.Product import Product
-        from models.subCategory import Subcategory
-        from models.order_item import OrderItem
-        from models.Order import Order
-        from models.Payment import Payment
-        from models.CartItem import CartItem
-        from models.Cart import Cart
-        from models.Review import Review
-        from models.users import User
-        mapp = {
-            "Category": Category,
-            "Subcategory": Subcategory,
-            "Discount": Discount,
-            "VaiationCombination": VaiationCombination,
-            "VaiationKey": VaiationKey,
-            "VaiationValue": VaiationValue,
-            "Product": Product,
-            "OrderItem": OrderItem,
-            "Order": Order,
-            "Payment": Payment,
-            "CartItem": CartItem,
-            "Cart": Cart,
-            "Review": Review,
-            "User": User,
-        }
         if str(cls) in mapp:
             return self.__session.query(mapp[str(cls)]).all()
         else:
@@ -82,38 +75,8 @@ class DBstorage:
     def delete(self, obj):
         self.__session.delete(obj)
         self.__session.commit()
+
     def get(self, cls, id):
-        from models.tracking import Tracking
-        from models.Category import Category
-        from models.Discount import Discount
-        from models.Comb import VaiationCombination
-        from models.variationsKey import VaiationKey
-        from models.variationsValue import VaiationValue
-        from models.Product import Product
-        from models.subCategory import Subcategory
-        from models.order_item import OrderItem
-        from models.Order import Order
-        from models.Payment import Payment
-        from models.CartItem import CartItem
-        from models.Cart import Cart
-        from models.Review import Review
-        from models.users import User
-        mapp = {
-            "Category": Category,
-            "Subcategory": Subcategory,
-            "Discount": Discount,
-            "VaiationCombination": VaiationCombination,
-            "VaiationKey": VaiationKey,
-            "VaiationValue": VaiationValue,
-            "Product": Product,
-            "OrderItem": OrderItem,
-            "Order": Order,
-            "Payment": Payment,
-            "CartItem": CartItem,
-            "Cart": Cart,
-            "Review": Review,
-            "User": User,
-        }
         if str(cls) in mapp:
             return self.__session.query(mapp[str(cls)]).get(id)
         else:
